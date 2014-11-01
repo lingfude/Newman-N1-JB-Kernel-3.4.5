40de62c1abff7cdd4e70eb71a2731f7d46762dbf
lingfude <ling.fude@gmail.com>
2014-11-01 22:01:07 +0000

    Initial commit

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Fri May 23 22:38:37 WEST 2014"
+#define LINUX_COMPILE_BY "lingfude"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.6.4 (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
---
+++ kernel/include/generated/utsrelease.h
@@ -0,0 +1,3 @@
+#define UTS_RELEASE "3.4.5-LingFude_1.3"
+#define BUILD_INFO "1.1 20140508-22:42"
+#define BUILD_FINGERPRINT "newmann1"
